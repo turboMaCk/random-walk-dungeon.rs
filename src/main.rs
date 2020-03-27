41d7fd5da878e18cd614b06b23216f20cc71ac1d
Marek Fajkus <marek.faj@gmail.com>
2020-03-27 14:38:15 +0100

    refactor main

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,20 @@ extern crate random_walk;
 use clap::{Arg, App, ArgMatches};
 use rand::{thread_rng};
 
+fn res_to_opt<T,E>(res: Result<T,E>) -> Option<T> {
+    res.map_or_else(|_| None, |x| Some(x))
+}
+
 fn main() {
     let mut dimensions: usize = 20;
     let mut tunnels_count: usize = 50;
     let mut max_tunnel_length: usize = 8;
     let mut rng = thread_rng();
+
+    // String values of args
+    let dimensions_str = dimensions.to_string();
+    let tunnels_count_str = tunnels_count.to_string();
+    let max_tunnel_length_str = max_tunnel_length.to_string();
 
     let matches: ArgMatches = App::new("Random Walk Dungeon")
         .version("0.1.0")
@@ -16,36 +25,36 @@ fn main() {
         .arg(Arg::with_name("dimensions")
              .long("dimensions")
              .short("d")
-             .default_value("20")
+             .default_value(&dimensions_str)
              .value_name("natural number")
              .help("Number of tails per side.")
         )
         .arg(Arg::with_name("tunnels")
              .long("tunnels")
              .short("t")
-             .default_value("50")
+             .default_value(&tunnels_count_str)
              .value_name("natural number")
              .help("Number of tunnels to digg.")
         )
         .arg(Arg::with_name("tunnel_len")
              .long("tunnel-length")
              .short("l")
-             .default_value("8")
+             .default_value(&max_tunnel_length_str)
              .value_name("natural number")
              .help("Maximal length of tunnel in tails.")
         )
         .get_matches();
 
     matches.value_of("dimensions")
-        .and_then(|v| v.parse::<usize>().map_or_else(|_| None, |x| Some(x)))
+        .and_then(|v| res_to_opt(v.parse::<usize>()))
         .map(|v| dimensions = v);
 
     matches.value_of("tunnels")
-        .and_then(|v| v.parse::<usize>().map_or_else(|_| None, |x| Some(x)))
+        .and_then(|v| res_to_opt(v.parse::<usize>()))
         .map(|v| tunnels_count = v);
 
     matches.value_of("tunnels_len")
-        .and_then(|v| v.parse::<usize>().map_or_else(|_| None, |x| Some(x)))
+        .and_then(|v| res_to_opt(v.parse::<usize>()))
         .map(|v| max_tunnel_length = v);
 
     random_walk::ascii_render_map(&random_walk::generate_map(&mut rng, dimensions, tunnels_count, max_tunnel_length));
